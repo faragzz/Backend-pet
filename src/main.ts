feede9a713546f58d9914f281767cada86f474b7
faragzz <a.khaled46462@gmail.com>
2025-02-25 19:53:07 +0200

    fix backend

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ import { join } from 'path';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true, transform: true }));
-  app.useStaticAssets(join(__dirname, '..' ,'..', 'uploads'), { prefix: '/uploads' });
+  // app.useStaticAssets(join(__dirname, '..' ,'..', 'uploads'), { prefix: '/uploads' });
   await app.listen(process.env.PORT ?? 3000);
 }
 
