855663ec81bcaf6670bd797741b7c74c76f63664
faragzz <a.khaled46462@gmail.com>
2025-04-15 17:33:31 +0200

    feat:posts

--- src/posts/dto/create.dto.ts
+++ src/posts/dto/create.dto.ts
@@ -1,13 +1,14 @@
 import { IsString, IsNumber, IsArray, ArrayNotEmpty, ValidateNested, IsDefined } from 'class-validator';
 import { Pet } from '../../pet/schema/pet.schema';
 import { Type } from 'class-transformer';
+import { CreatePetDto } from '../../pet/dto/create.dto';
 
 class CreatePostDto {
 
   @IsDefined()
   @ValidateNested()
-  @Type(() => Pet)
-  pet: Pet;
+  @Type(() => CreatePetDto)
+  pet: CreatePetDto;
 
   @IsString()
   title: string;
